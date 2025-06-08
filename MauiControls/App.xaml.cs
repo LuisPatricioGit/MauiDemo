f8d8c89916b1161e91dbbaa297392c0691debd66
LuisPatricioGit <luispatricio.info@gmail.com>
2025-06-08 13:55:24 +0100

    MAUI Presentation Controls

---
+++ MauiControls/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiControls.Pages;
+
+namespace MauiControls
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MauiPresentationControls());
+        }
+
+    }
+}
--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -11,7 +11,7 @@
             //navPage.BarBackground = Colors.Red;
             //navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new NavigationPage(new FlexLayoutDemo());
+            MainPage = new NavigationPage(new AbsoluteLayoutDemo());
         }
 
         //protected override Window CreateWindow(IActivationState? activationState)
